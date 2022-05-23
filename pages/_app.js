a91f0ddc55af97464bc3653fbd4ad72841fa9fb1
石倉圭悟 <ishikurakeigo@ishikurakeigonoMacBook-Air.local>
2022-05-23 15:44:54 +0900

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import '../styles/global.scss'
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      <h1>
+         hello world!
+      </h1>
+      </main>
+
+      <footer>
+       
+      </footer>
+
+      
+    </div>
+  )
+}
