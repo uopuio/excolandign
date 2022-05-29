63ae518d81dff3f10b25d78b2df0bf44e1d1e8b5
uopuio <81293878+uopuio@users.noreply.github.com>
2022-05-29 13:59:07 +0900

    header

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,30 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
 import '../styles/global.scss'
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Exco-ウェブサイトリニューアルのことならお任せください</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-      <h1>
-         hello world!
-      </h1>
+    <header>
+      <section>
+      <Image src='/excoLogo.png' alt='logo' width='131px' height='60px' style='width:131px!important height:60px!important;'/>
+      </section>
+      <nav className='navbar'>
+        <ul>
+          <li><a>サポート</a></li>
+          <li><a>実績</a></li>
+          <li><a>導入までの流れ</a></li>
+        </ul>
+      </nav>
+      <div className='button'>お問い合わせ</div>
+    </header>
+    
       </main>
 
       <footer>
